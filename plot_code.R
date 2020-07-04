70e0539d5ee8e0b43a339111cee42a1d6f916315
candaceng <candacewyng@gmail.com>
2020-07-04 08:01:53 -0400

    Plot 3 code and image

--- plot_code.R
+++ plot_code.R
@@ -8,8 +8,19 @@ png("plot1.png", width=480, height=480)
 with(yearly_emissions, plot(x = year, y = x, type="l", ylab = "Total Annual Emissions (tons)",xlab = "Year", main = "Total Emissions in the US by Year", col = "red"))
 dev.off()
 
-#Plot 2
+# Plot 2
 baltimore <- subset(NEI, NEI$fips=="24510")
 baltimore_yearly <- aggregate(baltimore$Emissions, by=list(baltimore$year), FUN=sum)
 png("plot2.png", width=480, height=480)
 with(baltimore_yearly, plot(x = Group.1, y = x, type="l", ylab="Total Annual Emissions (tons)", xlab="Year", main="Total Emissions in Baltimore by Year", col="blue"))
+
+# Plot 3
+library(ggplot2)
+baltimore_type <- aggregate(baltimore$Emissions, by=list(baltimore$type, baltimore$year), FUN=sum)
+colnames(baltimore_type) <- c("Type", "Year", "Emissions")
+png("plot3.png", width=480, height=480)
+qplot(Year, Emissions, data=baltimore_type, color=Type, geom="line")
+    + ggtitle("Total Emissions by Type")
+    + ylab("Total Emissions (tons)")
+    + xlab("Year")
+dev.off()
