02e600720cb443cab70053ae787c6ada9c971eb8
candaceng <candacewyng@gmail.com>
2020-07-04 08:17:30 -0400

    Plot 4 code and image

--- plot_code.R
+++ plot_code.R
@@ -13,6 +13,7 @@ baltimore <- subset(NEI, NEI$fips=="24510")
 baltimore_yearly <- aggregate(baltimore$Emissions, by=list(baltimore$year), FUN=sum)
 png("plot2.png", width=480, height=480)
 with(baltimore_yearly, plot(x = Group.1, y = x, type="l", ylab="Total Annual Emissions (tons)", xlab="Year", main="Total Emissions in Baltimore by Year", col="blue"))
+dev.off()
 
 # Plot 3
 library(ggplot2)
@@ -23,4 +24,13 @@ qplot(Year, Emissions, data=baltimore_type, color=Type, geom="line")
     + ggtitle("Total Emissions by Type")
     + ylab("Total Emissions (tons)")
     + xlab("Year")
+dev.off()
+
+# Plot 4
+coal <- SCC[grepl("Coal", SCC$Short.Name),]
+coal_data <- NEI[NEI$SCC %in% coal$SCC,]
+coal_yearly <- aggregate(coal_data$Emissions, by=list(coal_data$year), FUN=sum)
+colnames(coal_yearly) <- c("Year", "Emissions")
+png("plot4.png", width=480, height=480)
+plot(coal_yearly$Year, coal_yearly$Emissions, xlab="Year", type="h", ylab="Emissions", main="Annual Coal Related Emissions", col="red")
 dev.off()
